4924b50412e5efaf56de088b48dcfd2eee0d1537
Caleb <aragothofdagobah@gmail.com>
2016-09-06 00:21:04 -0400

    added js functions

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
+var windowHeight = $(window).height();
+var windowWidth = $(window).width();
+function autoHide(li) {
+  $("#list").fadeOut();
+  $(".menu-bar").animate({
+        transform: '',
+        left: windowWidth - 50,
+        top: 50,
+        opacity: '0.5',
+        height: '100px',
+        width: '100px'
+    });
+  if (li.id == "Work")
+    $(".Work").fadeIn();
+  if (li.id == "About")
+    $(".About").fadeIn();
+  if (li.id == "Contact")
+    $(".Contact").fadeIn();
+}
+
+
 $(document).ready(function(){
   $("#Work").click(function(){
-    $(".container").fadeOut();
-    $(".Work").fadeIn();
+    autoHide(this);
   });
   $("#About").click(function(){
-    $(".container").fadeOut();
-    $(".About").fadeIn();
+    autoHide(this);
   });
   $("#Contact").click(function(){
-    $(".container").fadeOut();
-    $(".Contact").fadeIn();
+    autoHide(this);
   });
 });
